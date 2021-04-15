cb9fe5e2fedaee2b87f2d133d694e55dfa60d07a
Lisdima <65314260+Lisdima@users.noreply.github.com>
2021-04-15 20:11:30 +0300

    Added project files

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import "./styles/style.scss";
+
+$(document).ready(function () {
+  $(".bxslider").bxSlider({
+    // controls: false,
+    nextText: '',
+    prevText: '',
+    useCSS: false,
+  });
+});
+
+
+$('img.icon-quotes').each(function(){
+  var $img = $(this);
+  var imgClass = $img.attr('class');
+  var imgURL = $img.attr('src');
+  $.get(imgURL, function(data) {
+    var $svg = $(data).find('svg');
+    if(typeof imgClass !== 'undefined') {
+      $svg = $svg.attr('class', imgClass+' replaced-svg');
+    }
+    $svg = $svg.removeAttr('xmlns:a');
+    if(!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+      $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+    }
+    $img.replaceWith($svg);
+  }, 'xml');
+});
